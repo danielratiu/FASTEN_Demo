76516af4f64c044367b19b58637b7bf54ca4f425
danielratiu <ratiud@googlemail.com>
2024-11-23 22:44:48 +0100

    initial content in the FASTEN project

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,20 +6,24 @@ import java.util.Date
 
 plugins {
     base
-    `maven-publish`
     id("co.riiid.gradle") version "0.4.2"
-
     val mpsGradlePluginVersion = "1.28.0.+"
+    id("download-jbr") version mpsGradlePluginVersion
     id("de.itemis.mps.gradle.common") version mpsGradlePluginVersion
 }
 
-val fastenVersion = "feature-publish_rcp_as_maven_packages.2023.2.2024.e589075"
+// detect if we are in a CI build
+val ciBuild = (System.getenv("CI") != null && System.getenv("CI").toBoolean()) || project.hasProperty("forceCI") || project.hasProperty("teamcity")
 
-fun appendOpt(str:String, pre:String) = if(!str.isEmpty()) "${pre}${str}" else ""
+
+val _mpsVersion = "2023.2"
+val fastenVersion = "2023.2.2032.27ca1f0"
+val rcpRepo = if (ciBuild) "linux.rcp" else "win.rcp"
 
 configurations {
+    val mps by creating
     dependencies {
-	fasten "fasten:win.rcp:$fastenVersion"
+	    mps("fasten:$rcpRepo:$fastenVersion")
     }
 }
  
@@ -31,7 +35,6 @@ repositories {
     for (repoUrl in dependencyRepositories) {
         maven {
             url = uri(repoUrl)
-
             if (repoUrl.startsWith("https://maven.pkg.github.com/")) {
                 credentials {
                     username = project.property("gpr.user") as String
@@ -43,14 +46,52 @@ repositories {
     mavenCentral()
 }
 
-modelcheck {
-    //modules = listOf("module-to-include")
-    //excludeModules = listOf("module-to-include")
-    //models = listOf("models-to-include")
-    //excludeModels = listOf()
-    parallel = true
-    maxHeap = "3G"
-    junitFormat = "module-and-model"
+val jbrVers = "17.0.8.1-b1000.32"
+downloadJbr {
+    jbrVersion = jbrVers
 }
 
-defaultTasks("check_models")
+
+val skipResolveMps = project.hasProperty("mpsHomeDir")
+val mpsHomeDir = rootProject.file(project.findProperty("mpsHomeDir")
+    ?: layout.buildDirectory.dir("mps").get().asFile.path)
+
+val resolveMps = if (skipResolveMps) {
+        tasks.register("resolveMps") {
+            doLast {
+                logger.info("MPS resolution skipped")
+                logger.info("MPS home: {}", mpsHomeDir.getAbsolutePath())
+            }
+        }
+    } else {
+        tasks.register("resolveMps", Copy::class) {
+            dependsOn(configurations["mps"])
+            from({
+                configurations["mps"].resolve().map(::zipTree)
+            })
+            into(mpsHomeDir)
+        }
+    }
+
+tasks {
+    withType<MpsCheck>().configureEach {
+        dependsOn(downloadJbr, resolveMps)
+        //javaLauncher = downloadJbr.flatMap { it.javaLauncher }
+
+        mpsVersion = _mpsVersion
+        mpsHome = mpsHomeDir
+        folderMacros.put("fasten.demo.home", layout.projectDirectory)
+        pluginRoots.addAll(
+            layout.buildDirectory.map { buildDir ->
+                listOf("mps/plugins").map { buildDir.dir(it) }
+            })
+        ignoreFailures = true
+        maxHeapSize = "2G"
+    }
+}
+
+tasks.register("runFastenModelChecks", MpsCheck::class) {
+    projectLocation = file(".")
+    modules = listOf("fasten.assurance.demo")
+}
+defaultTasks("runFastenModelChecks")
---
+++ solutions/fasten.assurance.demo/fasten.assurance.demo.msd
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<solution name="fasten.assurance.demo" uuid="04bf8f55-2f18-4294-b505-b258ba4141dc" moduleVersion="0">
+  <models>
+    <modelRoot type="default" contentPath="${module}">
+      <sourceRoot location="models" />
+    </modelRoot>
+  </models>
+  <facets>
+    <facet compile="mps" classes="mps" ext="no" type="java">
+      <classes generated="true" path="${module}/classes_gen" />
+    </facet>
+  </facets>
+  <languageVersions />
+  <dependencyVersions>
+    <module reference="04bf8f55-2f18-4294-b505-b258ba4141dc(fasten.assurance.demo)" version="0" />
+  </dependencyVersions>
+</solution>
+
