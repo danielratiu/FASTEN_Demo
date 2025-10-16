1d3eb3d01d1a70b0a71a6204ff0028180c850cd3
ratiud <extern.daniel.ratiu@volkswagen.de>
2025-10-16 23:06:43 +0200

    build.gradle.kts: updated FASTEN version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 // detect if we are in a CI build
 val ciBuild = (System.getenv("CI") != null && System.getenv("CI").toBoolean()) || project.hasProperty("forceCI") || project.hasProperty("teamcity")
 
-val fastenVersion = "2023.2.2222.d241e80"
+val fastenVersion = "2024.1.1518.fef85a7"
 val rcpRepo = if (ciBuild) "linux.rcp" else "win.rcp"
 
 configurations {
